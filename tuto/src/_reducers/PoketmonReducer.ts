8308065409268e2e882d21fb9c661d1970c8a0c4
류길수 <rlftn510@naver.com>
2021-01-26 20:15:56 +0900

    프로젝트 생성 및 reducer 셋팅

---
+++ tuto/src/_reducers/PoketmonReducer.ts
@@ -0,0 +1,17 @@
+interface initialState {
+
+}
+
+const initialState: initialState = {
+
+}
+
+const PoketmonReducer = ( state = initialState, action: any) => {
+   switch (action.type) {
+
+      default:
+         return state
+   }
+}
+
+export default PoketmonReducer
---
+++ tuto/src/_reducers/index.ts
@@ -0,0 +1,8 @@
+import PoketmonReducer from './PoketmonReducer'
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+   PoketmonReducer
+})
+
+export default rootReducer
