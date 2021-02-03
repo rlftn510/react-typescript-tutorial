10f26686a6ddbd37dc3104d0dbe901cb00c60178
류길수 <rlftn510@naver.com>
2021-02-03 18:54:49 +0900

    카트 기능 개발

--- react-shopping-cart/src/App.tsx
+++ react-shopping-cart/src/App.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { Component, useState } from 'react'
 import { useQuery } from 'react-query'
-import Item from './Item/Item'
 import Drawer from '@material-ui/core/Drawer'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import Grid from '@material-ui/core/Grid'
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge'
 
+// Component
+import Item from './Item/Item'
+import Cart from './Cart/Cart'
+
 import { Wrapper, StyledButton } from './App.styles'
+import { ArtTrack } from '@material-ui/icons'
 
 //types
 
@@ -27,16 +31,46 @@ const getProduct = async (): Promise<CartItemType[]> => {
 
 const App = () => {
    const [cartOpen, setCartOpen] = useState(false)
-   const [carItems, setCarItems] = useState([] as CartItemType[])
+   const [cartItems, setCartItems] = useState([] as CartItemType[])
    const { data, isLoading, error } = useQuery<CartItemType[]>(
       'products',
       getProduct
    )
 
-   const handleAddToCart = (clickedItem: CartItemType) => null
+   
    // const 
    const getTotalItems = (items: CartItemType[]) => {
       return items.reduce((ack: number, item) => ack + item.amount, 0)
+   }
+
+   const handleAddToCart = (clickedItem: CartItemType) => {
+      setCartItems(prev => {
+         const isItemInCart = prev.find(item => item.id === clickedItem.id)
+
+         if (isItemInCart) {
+            return prev.map(item => (
+               item.id === clickedItem.id
+                  ? { ...item, amount: item.amount + 1}
+                  : item
+            ))
+         }
+
+         return [...prev, { ...clickedItem, amount: 1}]
+      })
+   }
+   const handleRemoveFromCart = (id: number) => {
+      setCartItems(prev => {
+         return prev.reduce((ack, item) => {
+            if (item.id === id) {
+              if (item.amount === 1) return ack;
+              return [...ack, { ...item, amount: item.amount - 1 }];
+            } else {
+              return [...ack, item];
+            }
+          }, [] as CartItemType[])
+      }
+         
+       );
    }
 
    if (isLoading) return <LinearProgress />
@@ -48,10 +82,14 @@ const App = () => {
    return (
       <Wrapper>
          <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
-            Cart gose open
+            <Cart 
+               cartItems={cartItems} 
+               addToCart={handleAddToCart}
+               removeFromCart={handleRemoveFromCart}
+            />
          </Drawer>
          <StyledButton onClick={() => setCartOpen(true)}>
-            <Badge badgeContent={getTotalItems()} color="error">
+            <Badge badgeContent={getTotalItems(cartItems)} color="error">
                <AddShoppingCartIcon />
             </Badge>
          </StyledButton>
