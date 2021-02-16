483cca3cda132de588185ba89e3e7a99eb3d56d7
류길수 <rlftn510@naver.com>
2021-02-16 20:15:40 +0900

    route 기본

--- react-type-seminar/react-seminar/src/App.tsx
+++ react-type-seminar/react-seminar/src/App.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, RouteComponentProps } from 'react-router-dom'
+
+const Post = (props: RouteComponentProps<{postId: string}>) => {
+   const nextBtn = (): void => {
+      const nextId = parseInt(props.match.params.postId) + 1
+      props.history.push(`/posts/${nextId}`)
+   }
+   console.log(props)
+   return (
+      <>
+      <h2>post: {props.match.params.postId}</h2>
+      <p>parms : {new URLSearchParams(props.location.search).get('body')}</p>
+      <button onClick={nextBtn}>다음</button>
+      </>
+
+   )
+}
+
+const PostList = (props: RouteComponentProps<{}>) => {
+   return (
+      <div>
+         <Route exact path={`${props.match.url}`} component={()=> <h2>postList</h2>}/>
+         <Route path={`${props.match.url}/:postId`} component={Post}/>
+      </div>
+   )
+}
 
 function App() {
   return (
+     <Router>
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{height: '50vh'}}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <nav>
+         <ul>
+            <li><Link to="/">home</Link></li>
+            <li><Link to="/intro">intro</Link></li>
+         </ul>
+      </nav>
+      <Route exact path="/" render={() => <h2>hello</h2>}/>
+      <Route path="/intro" render={() => <h2>intro</h2>}/>
+      <Route path="/posts" component={PostList}/>
     </div>
+    </Router>
   );
 }
 
