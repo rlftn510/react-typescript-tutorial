60fbe2ec5d6ff5ff74695e60f93aeefc1f8e76db
류길수 <rlftn510@naver.com>
2021-02-22 19:12:15 +0900

    mobx class -> 객체형으로 변환 뒤 나이 변화

--- react-mobx/ts-mobx/src/App.tsx
+++ react-mobx/ts-mobx/src/App.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { inject, observer } from "mobx-react";
+import { inject, observer, useObserver } from "mobx-react";
 
-import store from './Store'
+import {store} from './Store'
 
-@observer 
-class App extends React.Component<{}, {}> {
-   render() {
-     return (
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           {store.age}
-        </p>
-        <button onClick={() => store.addAge()}>나이먹음</button>
-      </header>
-    </div>
-     );
-   }
-}
-
-// function App() {
-//   return (
-//     <div className="App">
+// @observer 
+// class App extends React.Component<{}, {}> {
+//    render() {
+//      return (
+//       <div className="App">
 //       <header className="App-header">
 //         <img src={logo} className="App-logo" alt="logo" />
 //         <p>
@@ -33,7 +18,24 @@ class App extends React.Component<{}, {}> {
 //         <button onClick={() => store.addAge()}>나이먹음</button>
 //       </header>
 //     </div>
-//   );
+//      );
+//    }
 // }
+
+function App() {
+  return useObserver(() => (
+   
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+             {store.age}
+          </p>
+          <button onClick={() => store.addAge()}>나이먹음</button>
+        </header>
+      </div>
+   
+  ))
+}
 
 export default App;
--- react-mobx/ts-mobx/src/Store.tsx
+++ react-mobx/ts-mobx/src/Store.tsx
@@ -1,13 +1,24 @@
 import { observable } from 'mobx'
 
-class Store {
-   @observable
-   age: number = 25;
-
-   public addAge(): void {
-      console.log(this.age)
-      this.age = this.age + 1;
-  }
+interface Todo {
+   age: number;
+   addAge: () => void
 }
 
-export default new Store()
+export const store = observable<Todo>({
+   age: 25,
+   addAge() {
+      this.age++
+   }
+})
+// class Store {
+//    @observable
+//    age:number = 25
+
+//    public addAge(): void {
+//       console.log(this.age)
+      
+//   }
+// }
+
+// export default new Store()
