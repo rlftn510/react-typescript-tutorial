3ec8bb6691e2bf48ea4831bfe46f49e81ce804ea
류길수 <rlftn510@naver.com>
2021-03-15 19:30:00 +0900

    .eslintcache

--- react-shopping-cart/src/_reducers/index.ts
+++ react-shopping-cart/src/_reducers/index.ts
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import ProductReducer from './ProductReducer'
+import ProductReducer from './productReducer'
 const rootReducer = combineReducers({
    ProductReducer
 })
